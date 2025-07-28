779f775192359b765a7e58fadb67c2f0916a981e
diegoagorasites <diegoagorasites@gmail.com>
2025-07-27 23:53:45 -0300

    Delete renderer directory

--- renderer/preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  abrirConfig: () => ipcRenderer.invoke('abrir-config'),
-  salvarConfig: (config) => ipcRenderer.invoke('salvar-config', config),
-  onProcessoStatus: (callback) => ipcRenderer.on('processo-status', callback)
-});
